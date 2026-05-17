d6f293c820d3351d2eaf12207a8fd791b0a8aa05
Corey Hines <chi125@uclive.ac.nz>
2026-05-17 13:35:03 +1200

    fix(draw): subscribe to drawing events as spectator

--- src/features/game/components/drawing/panel/spectator-panel.tsx
+++ src/features/game/components/drawing/panel/spectator-panel.tsx
@@ -1,8 +1,22 @@
 import { Canvas } from "@/features/game/components/drawing/canvas/canvas"
 import { useCanvasRenderer } from "@/features/game/hooks/canvas/use-canvas-renderer"
+import { useGameSocketDrawingEvents } from "@/features/game/hooks/socket/use-game-socket-drawing-events"
 
 export const SpectatorPanel = () => {
-  const { canvasRef, containerRef, canvasReady } = useCanvasRenderer()
+  const {
+    canvasRef,
+    containerRef,
+    canvasReady,
+    startRemoteStroke,
+    addRemotePoints,
+    commitRemoteStroke,
+  } = useCanvasRenderer()
+  useGameSocketDrawingEvents({
+    onRemoteStrokeStart: startRemoteStroke,
+    onRemoteStrokePoints: addRemotePoints,
+    onRemoteStrokeEnd: commitRemoteStroke,
+  })
+
   return (
     <div className="flex flex-1 p-3">
       <Canvas
