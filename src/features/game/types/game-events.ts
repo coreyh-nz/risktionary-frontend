af92ac2ef887b15fbfddc1a8d9cad4f23218ad8a
Corey Hines <chi125@uclive.ac.nz>
2026-05-20 17:15:56 +1200

    feat(game): implement round and volunteer states

--- src/features/game/types/game-events.ts
+++ src/features/game/types/game-events.ts
@@ -1,6 +1,10 @@
 import { GameState } from "./game"
+import { RoundState } from "./round"
 
-export type GameEventType = "STATE_CHANGED"
+export type GameEventType =
+  | "STATE_CHANGED"
+  | "ROUND_STATE_CHANGED"
+  | "VOLUNTEERS_UPDATED"
 
 export interface GameEvent {
   type: GameEventType
@@ -8,4 +12,12 @@ export interface GameEvent {
 
 export interface StateChangedEvent extends GameEvent {
   state: GameState
+}
+
+export interface RoundStateChangedEvent extends GameEvent {
+  state: RoundState
+}
+
+export interface VolunteersUpdatedEvent extends GameEvent {
+  volunteers: string[]
 }
